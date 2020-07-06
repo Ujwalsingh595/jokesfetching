c8a68dd55fe4356896bde641444e7eaf054c0d68
Ujwalsingh595 <ujwalsingh595@gmail.com>
2020-07-06 14:26:03 +0530

    Updates

---
+++ precache-manifest.01212618c98dfb22a4edd73ada615d8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89bf942b9fd36fc70500c565991ffb98",
+    "url": "/jokesfetching/index.html"
+  },
+  {
+    "revision": "5e9df238c535f7f6f1de",
+    "url": "/jokesfetching/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5ae8552573c37bfed1a3",
+    "url": "/jokesfetching/static/css/main.f737e219.chunk.css"
+  },
+  {
+    "revision": "5e9df238c535f7f6f1de",
+    "url": "/jokesfetching/static/js/2.eb1a324b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jokesfetching/static/js/2.eb1a324b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae8552573c37bfed1a3",
+    "url": "/jokesfetching/static/js/main.5f05835d.chunk.js"
+  },
+  {
+    "revision": "d9b690e9e07eeae43055",
+    "url": "/jokesfetching/static/js/runtime-main.f09643cf.js"
+  }
+]);
